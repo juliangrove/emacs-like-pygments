6461b95666959d49a74b211119390c83333f93e0
juliangrove <julian.grove@gmail.com>
2025-07-16 18:47:37 -0600

    Edit colors slightly.

--- el_gruvbox_light/style.py
+++ el_gruvbox_light/style.py
@@ -10,7 +10,7 @@ class ELGruvboxLight(Style):
     """
     
     # Base style settings - gruvbox-light colors
-    background_color = '#fbf1c7'  # bg0
+    background_color = '#f9f5d7'  # bg0_h
     default_style = '#3c3836'     # fg1
     highlight_color = '#ebdbb2'   # bg1
     line_number_color = '#928374' # gray
@@ -22,30 +22,30 @@ class ELGruvboxLight(Style):
         # Basic tokens
         Text:                      '#3c3836',  # fg1
         Whitespace:                '#928374',  # gray
-        Error:                     '#cc241d bold',  # red
+        Error:                     '#cc241d',  # red
         
         # Comments
-        Comment:                   '#928374 italic',  # gray
-        Comment.Multiline:         '#928374 italic',  # gray
-        Comment.Single:            '#928374 italic',  # gray
-        Comment.Special:           '#928374 italic bold',  # gray
-        Comment.Preproc:           '#8f3f71',  # purple
+        Comment:                   '#79740e',  # gray
+        Comment.Multiline:         '#79740e',  # gray
+        Comment.Single:            '#79740e',  # gray
+        Comment.Special:           '#79740e',  # gray
+        Comment.Preproc:           '#79740e',  # purple
         
         # Keywords
-        Keyword:                   '#9d0006 bold',  # dark red
-        Keyword.Constant:          '#9d0006 bold',  # dark red
-        Keyword.Declaration:       '#9d0006 bold',  # dark red
-        Keyword.Namespace:         '#9d0006 bold',  # dark red
-        Keyword.Pseudo:            '#9d0006 bold',  # dark red
-        Keyword.Reserved:          '#9d0006 bold',  # dark red
-        Keyword.Type:              '#b57614 bold',  # yellow
+        Keyword:                   '#9d0006',  # dark red
+        Keyword.Constant:          '#9d0006',  # dark red
+        Keyword.Declaration:       '#9d0006',  # dark red
+        Keyword.Namespace:         '#9d0006',  # dark red
+        Keyword.Pseudo:            '#9d0006',  # dark red
+        Keyword.Reserved:          '#9d0006',  # dark red
+        Keyword.Type:              '#b16286',  # yellow
         
         # Names
         Name:                      '#3c3836',  # fg1
         Name.Attribute:            '#af3a03',  # orange
         Name.Builtin:              '#076678',  # blue
         Name.Builtin.Pseudo:       '#076678',  # blue
-        Name.Class:                '#b57614 bold',  # yellow
+        Name.Class:                '#b16286',  # yellow
         Name.Constant:             '#8f3f71',  # purple
         Name.Decorator:            '#af3a03',  # orange
         Name.Entity:               '#af3a03',  # orange
@@ -71,7 +71,7 @@ class ELGruvboxLight(Style):
         
         # Operators
         Operator:                  '#3c3836',  # fg1
-        Operator.Word:             '#9d0006 bold',  # dark red
+        Operator.Word:             '#9d0006',  # dark red
         
         # Punctuation
         Punctuation:               '#3c3836',  # fg1
@@ -80,7 +80,7 @@ class ELGruvboxLight(Style):
         String:                    '#79740e',  # green
         String.Backtick:           '#79740e',  # green
         String.Char:               '#79740e',  # green
-        String.Doc:                '#928374 italic',  # gray
+        String.Doc:                '#928374',  # gray
         String.Double:             '#79740e',  # green
         String.Escape:             '#af3a03',  # orange
         String.Heredoc:            '#79740e',  # green
@@ -97,13 +97,13 @@ class ELGruvboxLight(Style):
         # Generic tokens (for diffs, etc.)
         Generic:                   '#3c3836',  # fg1
         Generic.Deleted:           '#cc241d',  # red
-        Generic.Emph:              '#3c3836 italic',  # fg1
-        Generic.Error:             '#cc241d bold',  # red
-        Generic.Heading:           '#3c3836 bold',  # fg1
+        Generic.Emph:              '#3c3836',  # fg1
+        Generic.Error:             '#cc241d',  # red
+        Generic.Heading:           '#3c3836',  # fg1
         Generic.Inserted:          '#79740e',  # green
         Generic.Output:            '#3c3836',  # fg1
-        Generic.Prompt:            '#3c3836 bold',  # fg1
-        Generic.Strong:            '#3c3836 bold',  # fg1
-        Generic.Subheading:        '#3c3836 bold',  # fg1
+        Generic.Prompt:            '#3c3836',  # fg1
+        Generic.Strong:            '#3c3836',  # fg1
+        Generic.Subheading:        '#3c3836',  # fg1
         Generic.Traceback:         '#076678',  # blue
     }
