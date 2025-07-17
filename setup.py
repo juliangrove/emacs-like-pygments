f426b38424e4e1ae8d0748a6b76a6beb776d6bcb
juliangrove <julian.grove@gmail.com>
2025-07-16 18:29:40 -0600

    Update.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name='el-gruvbox-light',
+setup(name='emacs-like-pygments',
       description='Emacs-like pygments color schemes for use with minted.',
       packages=find_packages(),
       entry_points="""
                    [pygments.styles]
-                   el-gruvbox-light = el-gruvbox-light.style:ELGruvboxLight
+                   el-gruvbox-light = el_gruvbox_light.style:ELGruvboxLight
                    """
       )
